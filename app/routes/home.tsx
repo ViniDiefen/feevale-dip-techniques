55bbccc0579865bd47e378ea7fdc79f4e875e9e5
ViniDiefen <viniciusdiefenbach04@gmail.com>
2026-09-02 14:59:41 +0000

    refactor: extract menu into AppMenu component
    - move menu rendering from home.tsx to app-menu.tsx
    - simplify home route to use AppMenu component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,4 @@
-import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
-  MenubarTrigger,
-} from "@/components/ui/menubar";
-import { menus, type Menu, type MenuEntry, type MenuItem } from "@/data/menu";
+import { AppMenu } from "@/components/app-menu";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
@@ -22,38 +13,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Menubar className="w-full rounded-none border-x-0 border-t-0 h-12">{menus.map(renderMenu)}</Menubar>;
-}
-
-function renderMenu(menu: Menu) {
-  return (
-    <MenubarMenu key={menu.label}>
-      <MenubarTrigger className="h-10 px-3">{menu.label}</MenubarTrigger>
-      <MenubarContent>{menu.items.map(renderMenuEntry)}</MenubarContent>
-    </MenubarMenu>
-  );
-}
-
-function renderMenuEntry(item: MenuEntry, index: number) {
-  if (item.type === "separator") {
-    return renderMenuSeparator(index);
-  }
-  if (item.type === "item") {
-    return renderMenuItem(item);
-  }
-}
-
-function renderMenuSeparator(index: number) {
-  return <MenubarSeparator key={index} />;
-}
-
-function renderMenuItem(item: MenuItem) {
-  const Icon = item.icon;
-  return (
-    <MenubarItem key={item.label} variant={item.variant} onClick={() => item.command.execute()}>
-      <Icon />
-      {item.label}
-      {item.shortcut && <MenubarShortcut>{item.shortcut}</MenubarShortcut>}
-    </MenubarItem>
-  );
+  return <AppMenu />;
 }
