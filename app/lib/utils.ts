7f0acb73b06d553d1cb870dfad0b7cbd4ac15ad2
ViniDiefen <viniciusdiefenbach04@gmail.com>
2026-09-05 12:21:43 +0000

    feat: add image validation utilities

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function isImageFile(file: File): boolean {
+  return file.type.startsWith("image/")
+}
+
+export function isUnderMaxSize(file: File, maxSizeMB: number): boolean {
+  return file.size <= maxSizeMB * 1024 * 1024
+}
